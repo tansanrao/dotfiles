cf7c0a3ab271acbab77271271b87e4948abd5c4c
Tanuj Ravi Rao <tanujrao99@gmail.com>
2024-07-24 11:03:56 -0400

    update zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,50 +1,39 @@
 # For when tmux attach causes problems with old agent in envvars
-fixssh() {
+function fixssh() {
 	eval $(tmux show-env -s |grep '^SSH_')
 }
 
 # remove from known_hosts helper for when VMs are torn down and rebuilt regularly
-rm_keys() {
+function rmkeys() {
 	ssh-keygen -f "/home/tansanrao/.ssh/known_hosts" -R $1
 }
 
-ssh-jump() {
-# Help message
-if [ $# -lt 2 ] || [ $# -gt 3 ]; then
-	echo "Usage: ssh-jump <username>@<host> <jumphost>"
-	echo "or:    ssh-jump <host> <jumphost>"
-	return 1
-fi
+function jump() {
+	if (( $# < 2 )); then
+		echo "Usage: jump host1 [host2 ... hostN]"
+		return 1
+	fi
 
-  # Grab hosts from arguments
-  host=$1
-  jump_host=$2
+	local hosts=("$@")
+	local last_host=${hosts[-1]}
+	local jump_hosts=${hosts[1,-2]}
 
-  # Execute ssh command
-  ssh -A -J $jump_host $host
+	local jump_string=""
+	for host in $jump_hosts; do
+		jump_string+="$host,"
+	done
+
+	jump_string=${jump_string%,}  # Remove trailing comma
+
+	ssh -J $jump_string $last_host
 }
 
 # Helper to list full trust chain for https server.
 function seecert () {
 	nslookup $1
-	(openssl s_client -showcerts -servername $1 -connect $1:443 <<< "Q" | openssl x509 -text | grep -iA2 "Validity")
-}
-
-ssh-jmp() {
-# Help message
-if [ $# -lt 2 ] || [ $# -gt 3 ]; then
-	echo "Usage: ssh-jump <username>@<host> <jumphost>"
-	echo "or:    ssh-jump <host> <jumphost>"
-	return 1
-fi
-
-	# Grab hosts from arguments
-	host=$1
-	jump_host=$2
-
-	# Execute ssh command
-	ssh -A -J $jump_host $host
-}
+	(openssl s_client -showcerts -servername $1 -connect $1:443 <<< \
+		"Q" | openssl x509 -text | grep -iA2 "Validity")
+	}
 
 # CS3754 helpers
 function 3754_unzip_all () {
