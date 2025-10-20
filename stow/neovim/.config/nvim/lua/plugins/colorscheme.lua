136255d6b5541e9fc7510327f53c15b5b7b2a627
Tanuj Ravi Rao <email@tansanrao.com>
2025-10-20 15:53:44 -0400

    light theme by default

--- stow/neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ stow/neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,23 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    vim.cmd("colorscheme rose-pine")
-  end,
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- optional: tweak here if you like
+      variant = "auto", -- main/moon/dawn auto picks by background
+      dark_variant = "main", -- "main" for classic Rosé Pine dark
+      styles = { transparency = false },
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      vim.o.background = "light"
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "rose-pine" },
+  },
 }
